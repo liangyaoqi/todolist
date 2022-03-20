2087a5fa2076b0f7fae5ff28851bbfde4b3dc698
‘liangyaoqi’ <>
2022-03-20 18:00:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Headers from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+import './App.css';
+
+
+export default class App extends Component {
+  render(){
+    return (
+    <div className="todo-container">
+    <div className="todo-wrap">
+      <Headers/>
+      <List/>
+      <Footer/>
+    </div>
+  </div>
+  );
+  }
+  
+}
+
+
+
